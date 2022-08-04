4aba9eba3a5c49d976be674647b355389a1cf11d
legonnexon <73261176+legonnexon@users.noreply.github.com>
2022-08-04 11:25:37 -0300

    Update load.js

--- load.js
+++ load.js
@@ -5,48 +5,46 @@ let borda = document.getElementById("borda");
 document.getElementById('btn').onclick = function(e) {
   borda.innerHTML = " ";
   let number = document.getElementById('number').value;
-  if (number > 100) { alert("Muito Grande") return 1 } 
-  else { 
-    let i = 0;
-    numberBooks = 0; //Verifica quantos livros tem
-    do {
-      var idBook = "livro" + i; //Vai ser usado para diferenciar os Ids do livros
-      var idText = "text" + i;
-      borda.innerHTML += livro; // Adicionar mais um livro
-      document.getElementById("livro").setAttribute("id", idBook); //Mudar o Id para o identificador
-      document.getElementById("text").setAttribute("id", idText); //Mudar o Id para o identificador
+  if (number > 100) { alert("Muito Grande") window.location.reload() } 
+  let i = 0;
+  numberBooks = 0; //Verifica quantos livros tem
+  do {
+    var idBook = "livro" + i; //Vai ser usado para diferenciar os Ids do livros
+    var idText = "text" + i;
+    borda.innerHTML += livro; // Adicionar mais um livro
+    document.getElementById("livro").setAttribute("id", idBook); //Mudar o Id para o identificador
+    document.getElementById("text").setAttribute("id", idText); //Mudar o Id para o identificador
 
-      // Uma cor aleatoria em HEX
-      let corAleatoria =
-        "#" +
-        parseInt(Math.random() * 0xffffff)
-          .toString(16)
-          .padStart(6, "0");
+    // Uma cor aleatoria em HEX
+    let corAleatoria =
+      "#" +
+      parseInt(Math.random() * 0xffffff)
+        .toString(16)
+        .padStart(6, "0");
 
-      document.getElementById(idBook).style.backgroundColor = corAleatoria; // Adiciona a cor
+    document.getElementById(idBook).style.backgroundColor = corAleatoria; // Adiciona a cor
 
-      //Verifica quando colocar a estante
-      if (numberBooks == 9) {
-        borda.innerHTML += base;
-        numberBooks = 0;
-      } else if (numberBooks != 9) {
-        numberBooks++;
+    //Verifica quando colocar a estante
+    if (numberBooks == 9) {
+      borda.innerHTML += base;
+      numberBooks = 0;
+    } else if (numberBooks != 9) {
+      numberBooks++;
+    }
+
+    // Make letras aleatorias
+    function makeid(length) {
+      var result = '';
+      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var charactersLength = characters.length;
+      for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
       }
+      return result;
+    }
 
-      // Make letras aleatorias
-      function makeid(length) {
-        var result = '';
-        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-      }
+    document.getElementById(idText).innerText = makeid(4);
 
-      document.getElementById(idText).innerText = makeid(4);
-
-      i++;
-    } while (i < number);
-  }
+    i++;
+  } while (i < number);
 }
